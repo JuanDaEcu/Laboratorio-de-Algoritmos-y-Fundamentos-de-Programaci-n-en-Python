065d0c4c68105d6ef4116e0cd70dbb5cf8cd145e
JuanDaEcu <153653287+JuanDaEcu@users.noreply.github.com>
2023-12-12 11:49:32 -0500

    Add files via upload

---
+++ Laboratorio de Algoritmos y Fundamentos de Programación en Python.py
@@ -0,0 +1,50 @@
+# 4.2:Agrega un comentario al inicio del programa que describa el propósito general del mismo
+# El proposito de este trabajo es aprender a programar y pensar como cientificos programadores para cada dia ir mejorando mas.
+# Ejercicio 1: Crea un programa en Python que realice las siguientes operaciones:
+# 1.1:Suma dos números enteros
+a_1 = 50
+b_1 = 25
+c_1 = a_1 + b_1
+print (c_1)
+# 1.2:Resta dos números de punto flotante
+a_2 = 25.5
+b_2 = 10.5
+c_2 = a_2 - b_2
+print (c_2)
+# 1.3:Multiplica dos números enteros
+a_3 = 10
+b_3 = 6
+c_3 = a_3 * b_3
+print (c_3)
+# 1.4:Divide dos números de punto flotante
+a_4 = 100.5
+b_4 = 10.5
+c_4 = a_4 / b_4
+print (c_4)
+
+# Ejercicio 2: Manipulación de Cadenas de Texto
+# 2.1:Declara una variable de tipo string que contenga tu nombre completo
+Nombre_1 = "Juan Daniel Guerrero"
+print ("Hola mi nombre es: "+Nombre_1+"")
+# 2.2:Utiliza la función len() para obtener la longitud de tu nombre completo y guárdalo en una variable
+Longitud = (print (len (Nombre_1) ) )
+# 2.3:Imprime en pantalla tu nombre completo y su longitud, utilizando la función print()
+print ("Mi nombre es: "+Nombre_1+"")
+# 2.4:Crea un comentario explicativo sobre el propósito de este programa
+Co = "El proposito de este programa es enseñar a programar de la manera mas facil y amigable posible"
+print ("Comentario: "+Co+"")
+
+# Ejercicio 3: Conversión de Tipos de Datos
+# 3.1:Declara una variable entera y otra de punto flotante
+a_5 = 20
+b_5 = 2.5
+# 3.2:Realiza la conversión de la variable entera a punto flotante y viceversa
+print (a_5)
+print (b_5)
+c_1 = float(a_5)
+d_1 = int(b_5)
+# 3.3:Imprime en pantalla el tipo de dato original y el tipo de dato después de la conversión
+print (a_5)
+print (b_5)
+print (c_1)
+print (d_1)
